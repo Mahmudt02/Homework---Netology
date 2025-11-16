220c649281fc08742f6d65e61e258cb94fb6a900
Mahmudt02 <Tojidinovtjk@gmail.com>
2025-11-16 15:21:32 +0300

    homework 2
    1212

---
+++ Homework2.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <clocale>
+
+int main() {
+
+    std::setlocale(LC_ALL, "Russian");
+
+    int A, B, C;
+
+   
+    std::cout << "¬ведите первое число: ";
+    std::cin >> A;
+    std::cout << "¬ведите второе число: ";
+    std::cin >> B;
+    std::cout << "¬ведите третье число: ";
+    std::cin >> C;
+
+    std::cout << "\n–езультат: ";
+
+ 
+    if (A >= B && A >= C) {
+        std::cout << A << " ";
+
+       
+        if (B >= C) {
+            std::cout << B << " " << C << std::endl; 
+        }
+        else {
+            std::cout << C << " " << B << std::endl; 
+        }
+    }
+    
+    else if (B >= A && B >= C) {
+        std::cout << B << " "; 
+
+        
+        if (A >= C) {
+            std::cout << A << " " << C << std::endl; 
+        }
+        else {
+            std::cout << C << " " << A << std::endl; 
+        }
+    }
+  
+    else {
+        std::cout << C << " "; 
+
+        
+        if (A >= B) {
+            std::cout << A << " " << B << std::endl; 
+        }
+        else {
+            std::cout << B << " " << A << std::endl; 
+        }
+    }
+
+    return 0;
+}
