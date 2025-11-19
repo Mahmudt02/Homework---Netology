724f586e1fa4d165ead98a9907b1bef3c9982ef9
Mahmudt02 <Tojidinovtjk@gmail.com>
2025-11-20 00:38:17 +0300

    Update Homework3.cpp

--- Homework3.cpp
+++ Homework3.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include <locale> 
 
+int main() {
+
+
+    std::setlocale(LC_ALL, "Russian");
+
+    int N; 
+
+    std::cout << "Введите целое число:\n";
+    std::cin >> N;
+    
+    for (int i = 1; i <= 10; i++) {
+
+        
+        
+        std::cout << N << " x " << i << " = " << (N * i) << std::endl;
+    }
+
+    return 0;
+}
