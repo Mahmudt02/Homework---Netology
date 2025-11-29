120261fc5f37e2ec3c5195a1835cdfd1df02d0b9
Mahmudt02 <Tojidinovtjk@gmail.com>
2025-11-29 17:54:00 +0300

    Update Homework2.cpp

--- Homework2.cpp
+++ Homework2.cpp
@@ -1,42 +1,29 @@
 #include <iostream>
-#include <limits> 
-#include <locale>
+#include <Locale>
 
-int main() {
-    
+void calculateAndPrintPower(int value, int power) {
+
     std::setlocale(LC_ALL, "Russian");
-
-    int array[10] = { 15, -3, 8, 20, 1, 9, 0, -10, 5, 12 };
-    const int SIZE = 10;
+    
+    int result = 1;
 
     
-    std::cout << "Массив: ";
-    for (int i = 0; i < SIZE; ++i) {
-        std::cout << array[i] << " ";
-    }
-    std::cout << std::endl;
-
- 
-
-    int min_element = array[0];
-    int max_element = array[0];
-
-   
-    for (int i = 1; i < SIZE; ++i) {
-     
-        if (array[i] < min_element) {
-            min_element = array[i]; 
-        }
-
-       
-        if (array[i] > max_element) {
-            max_element = array[i]; 
-        }
+    for (int i = 0; i < power; i++) {
+        result *= value;
     }
 
    
-    std::cout << "Минимальный элемент: " << min_element << std::endl;
-    std::cout << "Максимальный элемент: " << max_element << std::endl;
+    std::cout << value << " в степени " << power << " = " << result << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+    
+    calculateAndPrintPower(5, 2);
+
+    calculateAndPrintPower(3, 3);
+
+    calculateAndPrintPower(4, 4);
 
     return 0;
 }
