c4e299f6d6e82cd5a7d5e5c9d09bd5c4a20323f7
Mahmudt02 <Tojidinovtjk@gmail.com>
2025-12-13 14:06:18 +0300

    Update Homework.cpp

--- Homework.cpp
+++ Homework.cpp
@@ -1,23 +1,50 @@
 #include <iostream>
 #include <locale>
 
-void counting_function() {
-    std::setlocale(LC_ALL, "Russian");
-    static int count = 0;
 
-
-    count++;
-
-   
-    std::cout << "Количество вызовов функции counting_function(): " << count << std::endl;
+int calc(int x, int y) {
+    return x + y;
 }
 
-int main(int argc, char** argv)
-{
-    for (int i = 0; i < 10; i++)
-    {
-        counting_function();
-    }
+
+int calc(int x, double y) {
+    return x - (int)y;
+}
+
+
+int calc(double x, int y) {
+    return (int)x * y;
+}
+
+
+int calc(double x, double y) {
+
+    return (int)(x / y);
+}
+
+
+int main() {
+
+    int a = 4;
+    int b = 8;
+    std::setlocale(LC_ALL, "Russian");
+    std::cout << "x = " << a << ", y = " << b << std::endl;
+    std::cout << std::endl;
+
+
+    int result_sum = calc(a, b);
+    std::cout << "Сложение: " << result_sum << std::endl;
+
+
+    int result_sub = calc(a, (double)b);
+    std::cout << "Вычитание: " << result_sub << std::endl;
+
+
+    int result_mul = calc((double)a, b);
+    std::cout << "Умножение: " << result_mul << std::endl;
+
+    int result_div = calc((double)a, (double)b);
+    std::cout << "Деление: " << result_div << std::endl;
 
     return 0;
 }
